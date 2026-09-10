ed2b598bb16bdbedc8a556d538854bc7097aa154
witryna3210 <witryna.witryna@interia.pl>
2026-09-10 13:15:59 +0200

    Delete js directory

--- js/index.js
+++
@@ -1,7 +0,0 @@
-const nav = document.getElementsByTagName("nav")[0];
-const menuButton = document.getElementById("menu");
-
-menuButton.addEventListener("click", () => {
-  nav.classList.toggle("shown");
-  menuButton.classList.toggle("nav-item-active");
-});
